dee7a6239c8603d3884223e57e0cda147db7d5d4
Said Gadjiev <ackusha1984@ya.ru>
2022-12-22 20:38:28 +0300

    Добавление домашнего задания

---
+++ Task_47/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+int GetNumber(string messege)
+{
+    int resultat;
+    while (true)
+    {
+        Console.Write(messege);
+        if (int.TryParse(Console.ReadLine(), out resultat) && resultat > 0)
+        {
+            break;
+        }
+        else Console.WriteLine("Ввели не число");
+    }
+    return resultat;
+}
+
+void InitArray(int rows, int col)
+{
+    double[,] matrix = new double[rows, col];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = Convert.ToDouble(rnd.Next(-100,100)/10.0);
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int rows = GetNumber("Введите количество строк > ");
+int col = GetNumber("Введите количество столбцов - > ");
+InitArray(rows, col);
---
+++ Task_50/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+Задача 50. Напишите программу, которая на вход 
+принимает позиции элемента в двумерном массиве, и 
+возвращает значение этого элемента или же указание, 
+что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+int GetNumber(string messege)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.Write(messege);
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else Console.WriteLine("Не корректный ввод числа");
+    }
+    return result;
+}
+
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 20);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindNumber(int[,] matrix, int number)
+{
+    int count = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] == number)
+            {
+                Console.WriteLine($"Число {number} найден на позиции {i+1} {j+1}");
+                count++;
+            }
+        }
+    }
+    if (count == 0) Console.WriteLine(number + "-> такого числа в массиве нет");
+}
+
+int rows = GetNumber("Введите количество строк -> ");
+int columns = GetNumber("Введите количество столбцов -> ");
+int number = GetNumber("Введите число для поиска -> ");
+int[,] matrix = InitMatrix(rows, columns);
+
+PrintMatrix(matrix);
+Console.WriteLine();
+FindNumber(matrix, number);
